7f128c08c923897187b08bc277f5b2354adc9925
web-cubr <mishaoboronov@yandex.ru>
2021-03-09 21:33:38 +0200

    main

--- src/js/modules/defaults.js
+++ src/js/modules/defaults.js
@@ -1,7 +1,15 @@
 var defaults = {
 
 	events: () => {
-
+		$('.js-main-item').mouseover(function(){
+			let index = $(this).index();
+			$('.js-main-slide.is-active').removeClass('is-active');
+			$('.js-main-slide:eq(' + index + ')').addClass('is-active');
+			
+		}).mouseleave(function(){
+			$('.js-main-slide.is-active').removeClass('is-active');
+			$('.js-main-slide:first-child').addClass('is-active');
+		})
 	},
 	
 	header: (e) => {
